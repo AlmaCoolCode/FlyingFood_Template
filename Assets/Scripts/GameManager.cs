d89494d27fdb315f5af303fd92d89f303a77675e
AlmaCoolCode <alma@coolcode.io>
2024-12-10 18:25:33 +0100

    Restart Knopf angefangen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using JetBrains.Annotations;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private int timer = 121;
+    private int timer = 121;
+    [SerializeField] private int timerInsgesamt = 90;
     [SerializeField] private TMP_Text timerText;
     [SerializeField] private GameObject RestartPanel;
 
     private void Start()
     {
+        Time.timeScale = 1;
+        timer = timerInsgesamt;
         timerText.text = "Time left: " + timer.ToString();
         InvokeRepeating(nameof(CountDown), 1, 1);
         RestartPanel.SetActive(false);
@@ -27,6 +31,14 @@ public class GameManager : MonoBehaviour
             print("Time ran out!!!");
             Time.timeScale = 0;
             RestartPanel.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
         }
+        
+    }
+    public void Restart()
+    {
+        print("Hat geklappt");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        
     }
 }
